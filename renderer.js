debf42a110b78df80d5ab5248eae678394750373
Betty <nmydgm123@gmail.com>
2026-03-19 20:49:46 +0800

    fix: 添加微透明背景以便看到猫咪

--- renderer.js
+++ renderer.js
@@ -1,6 +1,7 @@
 const canvas = document.getElementById('petCanvas');
 canvas.width = window.innerWidth;
 canvas.height = window.innerHeight;
+canvas.style.backgroundColor = 'rgba(255, 250, 255, 0.1)';
 
 const pet = new Pet(canvas);
 const STATE_VERSION = 1;
